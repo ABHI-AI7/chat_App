dd60370722e105af9e30351590e395cd99d7a74c
ABHI-AI7 <asahlot7@gmail.com>
2025-03-07 16:20:19 +0530

    Backend setup complete

--- Back_End/src/routes/message.route.js
+++ Back_End/src/routes/message.route.js
@@ -1,11 +1,15 @@
 import express from "express";
-import { protectRoute } from "../middlelware/auth.middleware";
-import { getUsersForSidebar } from "../controllers/msessage.controller";
+import { protectRoute } from "../middlelware/auth.middleware.js";
+import { getMessages, getUsersForSidebar, sendMessages } from "../controllers/msessage.controller.js";
 
 const router = express.Router()
 
 router.get("/users", protectRoute, getUsersForSidebar);
 router.get("/:id", protectRoute, getMessages)
+
+router.post("/send", protectRoute, sendMessages)
+
+
 
 
 export default router
